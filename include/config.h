4fda658506b9a47b8cf5158cc800b0c5dd22bf28
Roshan Kattel <45326607+Roshankattel@users.noreply.github.com>
2026-06-02 16:38:03 +0545

    removed pins

--- include/config.h
+++ include/config.h
@@ -8,12 +8,6 @@
 #define MODEM_RING_PIN (40)
 #define MODEM_POWERKEY_PIN (33) // You have to turn off 4G from DIP swticbn before using this pin
 
-#define BOARD_LED_PIN (12)
-#define LED_ON (LOW)
-
-#define BOARD_BAT_ADC_PIN (4)
-#define BOARD_SOLAR_ADC_PIN (5)
-
 // SD Card pins
 #define SDMMC_CMD (4)
 #define SDMMC_CLK (5)
@@ -25,7 +19,5 @@
 #define MAX17048_I2C_ADDRESS 0x36
 
 #define RGB_PIN (38)
-
-
 
 #endif // CONFIG_H
